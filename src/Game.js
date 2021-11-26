95a7c8a4298f16a40fba9a6c55d4e0eb231e70d8
Hugoneytor <halvarez0@ucol.mx>
2021-11-26 16:37:51 -0600

    Documentacion agregada y valores de la tabla arreglados

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,13 @@
 module.exports = class Game{
+    //Declaracion de las propiedades del juego
     constructor(){
         this.strike = false;
         this.spare = false;
         this.turnos = 10;
         this.pinos = 10;
         this.tiros = 2;
+        this.resultado = 0;
+        //Declaracion del tablero con sus 10 turnos
         this.tablero = [
             {   //Turno 1
                 primerTiro:0,
@@ -60,32 +63,68 @@ module.exports = class Game{
         ];
     }
 
+    //Metodo Principal
     comenzarJuego(){
+        //Imprimimos la tabla en ceros
         console.table(this.tablero);
+        //Creacion de un for para recorrer los 10 turnos del juego
         for(let i = 0; i < this.turnos; i++){
+            //En cada iteracion se realiza el primer tiro
             this.primerTiro(this.tablero[i])
+            //Si el primer tiro es strike se salta al siguiente
             if(this.strike == true){
+                //Suma de los valores al tablero con solo el primer tiro
+                this.tablero[i].resultado = this.tablero[i].primerTiro + this.tablero[i].segundoTiro;
+                this.resultado += this.tablero[i].resultado;
+                //Devolver el valor del strike a falso
+                this.strike = false;
+                //Terminar con la iteracion
                 continue;
-            }else{
-                this.segundoTiro(this.tablero[i])
             }
+            //Realizacion del segundo tiro
+            this.segundoTiro(this.tablero[i])
+
+            //Suma de los resultados contando el segundo tiro
             this.tablero[i].resultado = this.tablero[i].primerTiro + this.tablero[i].segundoTiro;
+            this.resultado += this.tablero[i].resultado;
         }
+        //NUEVO METODO...
+        const resultados = actualizarResultados()
+
+        //Impresion de los resultados finales
         console.table(this.tablero);
+        console.log( this.resultado );
         
     }
+
+    //En curso...
+    actualizarResultados(){
+        for(let i = 0; i < this.tablero.length; i++){
+            if(this.tablero[i].primerTiro === 10){
+                
+            }
+        }
+    }
+
+    //Metodo del primer tiro
     primerTiro(tablero){
+        //Metodo aleatorio para obtener un numero del 1 al 10
         let primerTiro = Math.floor( Math.random()*this.pinos ) + 1;
+        //Agregar los valores a su iteracion
         tablero.primerTiro = primerTiro;
         this.pinos -= primerTiro;
+        //Validar si es un strike
         if(primerTiro == 10){
             this.strike = true;
             this.pinos = 10;
         }
     }
+    //Metodo del segundo tiro
     segundoTiro(tablero){
+        //Metodo aleatorio para obtener un numero del 1 al 10
         let segundoTiro = Math.floor( Math.random()*this.pinos ) + 1;
         tablero.segundoTiro = segundoTiro;
+        //Validar si es un spare
         if(segundoTiro == this.pinos){
             this.spare = true;
         }
